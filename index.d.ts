1252e64cb5189d1af9b0cf02be07538cbe0c0a33
FaridAA <faridabdul1406@gmail.com>
2023-08-23 21:53:01 +0700

    add typescript support

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+type Binascii = typeof binascii;
+
+declare namespace binascii{
+    export type hexlify = (data: string) => string;
+    export type unhexlify = (data: string) => string;
+    export type b2a_hex = (data: string) => string;
+    export type a2b_hex = (data: string) => string;
+
+    // named export
+    // import { plugin } from 'plugin'
+    // const { plugin } = require('plugin')
+    export const binascii: Binascii
+    // default export
+    // import plugin from 'plugin'
+    export {binascii as default}
+}
